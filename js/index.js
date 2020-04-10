23957f694a0b798f79cc71060bf87194998b8995
Kellybp <indeterminatarum@gmail.com>
2020-04-10 12:15:55 -0400

    Initial design structure

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//Display Image preview
+$("#img").on("change", function () {
+  if (typeof FileReader != "undefined") {
+    var image_holder = $("#preview");
+    image_holder.empty();
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("<img />", {
+        src: e.target.result,
+        class: "thumb-image",
+      }).appendTo(image_holder);
+    };
+    image_holder.show();
+    reader.readAsDataURL($(this)[0].files[0]);
+  } else {
+    alert("This browser does not support FileReader.");
+  }
+});
