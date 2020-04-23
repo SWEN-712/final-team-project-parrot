e6520a26d8e359d72305c65fd06122dbdb7b745d
Kellybp <indeterminatarum@gmail.com>
2020-04-23 10:26:59 -0400

    Organizing files/neatening

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # final-team-project-parrot
 
-hello world
-#log
-3/13/20
-Repository created
-Branches created
+<h2>How To:</h2>
+    <ol>
+        <li>Choose an image to upload (we do not keep any images)</li>
+        <li>
+        Upload the image via either drag and drop inside the blue dotted
+        section or by selecting "<strong>Choose File</strong>"<br />
+        Accepted images: All image types are accepted.
+        </li>
+        <li>
+        Select the "<strong>Run Generation</strong>" button to generate alt
+        text
+        </li>
+        <li>
+        The generated text comes from Google (nouns), IBM (full alt text),
+        and a machine learning alorithm (full alt text)
+        </li>
+        <li>
+        Use the section called "<strong>Build Your Alt Text</strong>" to
+        develop your alt text and copy it using the "<strong>
+            Copy Text</strong
+        >" button
+        </li>
+    </ol>
--- js/index.js
+++ js/index.js
@@ -20,15 +20,16 @@ const data = {
     },
   ],
 };
-const azureKey = '67c784f91bb84c77b9229d1abbee12c5';
-const azureUrl = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.1/analyze?visualFeatures=Description'
+const azureKey = "67c784f91bb84c77b9229d1abbee12c5";
+const azureUrl =
+  "https://westcentralus.api.cognitive.microsoft.com/vision/v2.1/analyze?visualFeatures=Description";
 let imagepath = "";
-
 let imageFile = "";
 
 // ########## Listeners
 //Display Image preview
 $("#inputImage").on("change", function () {
+  encodeBase64(this);
   if (typeof FileReader != "undefined") {
     var image_holder = $("#preview");
     image_holder.empty();
@@ -48,11 +49,15 @@ $("#inputImage").on("change", function () {
   }
 });
 
-$("#inputImage").on("change", function () {
-  encodeBase64(this);
+$("#uploadButton").on("click", function () {
+  getNounsGoogle();
+  getCaptionMicrosoft();
+  getCaptionIBM();
 });
 
-function fileImage(elm) {
+// ######### Functions
+//encode to Base64
+function encodeBase64(elm) {
   const file = elm.files[0],
     imgReader = new FileReader();
 
@@ -62,18 +67,41 @@ function fileImage(elm) {
   };
   imgReader.readAsDataURL(file);
 }
+function fileImage(elm) {
+  const file = elm.files[0],
+    imgReader = new FileReader();
 
-$("#uploadButton").on("click", function () {
-  getImageTags();
-  getCaptionMicrosoft();
-});
+  imgReader.onloadend = function () {
+    imagepath = imgReader.result.split(",")[1];
+  };
+  imgReader.readAsDataURL(file);
+}
 
-// $("#copy").on("click", function () {
-//   copyText();
-// });
+//Get IBM API data
+function getCaptionIBM() {
+  var output = document.getElementById("output");
+  document.getElementById("uploadButton").onclick = function () {
+    var data = new FormData();
+    data.append("image", document.getElementById("inputImage").files[0]);
 
-// ######### Functions
-function getImageTags() {
+    axios
+      .post(
+        "http://max-image-caption-generator.max.us-south.containers.appdomain.cloud/model/predict",
+        data
+      )
+      .then(function (res) {
+        var alttext = res.data.predictions[0].caption;
+        document.getElementById("IBMalt").value = alttext;
+      })
+      .catch(function (err) {
+        output.className = "container text-danger";
+        output.innerHTML = err.message;
+      });
+  };
+}
+
+//Get Google API data
+function getNounsGoogle() {
   data.requests[0].image.content = imagepath;
   axios({
     method: "POST",
@@ -89,52 +117,41 @@ function getImageTags() {
     .catch((err) => console.log(err));
 }
 
-function encodeBase64(elm) {
-  const file = elm.files[0],
-    imgReader = new FileReader();
-
-  imgReader.onloadend = function () {
-    // console.log('Base64 Format', imgReader.result);
-    imagepath = imgReader.result.split(",")[1];
-  };
-  imgReader.readAsDataURL(file);
-}
-
-// #### Tooltips
-function copyText() {
-  /* Get the text field */
-  var copyText = $("#altBuilding");
-
-  /* Focus on text */
-  copyText.select();
-
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  var tooltip = $("#myTooltip");
-  tooltip.html("Copied: " + copyText.val());
-}
-
-function outFunc() {
-  var tooltip = $("#myTooltip");
-  tooltip.html("Copy to clipboard");
-}
-
+//Get Mirosoft API data
 function getCaptionMicrosoft() {
   let formData = new FormData();
   formData.append("data", imageFile);
 
   axios({
-    method: 'POST',
+    method: "POST",
     url: azureUrl,
     headers: {
-      'Ocp-Apim-Subscription-Key': azureKey
+      "Ocp-Apim-Subscription-Key": azureKey,
     },
-    data: formData
-  }).then(res => {
-    if(res && res.data.description.captions[0]) {
-      $("#AzureAlt").val()
-      document.getElementById("AzureAlt").value = res.data.description.captions[0].text;
-    }
-  }).catch(e => console.log("Error getting Microsoft description", e));
+    data: formData,
+  })
+    .then((res) => {
+      if (res && res.data.description.captions[0]) {
+        $("#AzureAlt").val();
+        document.getElementById("AzureAlt").value =
+          res.data.description.captions[0].text;
+      }
+    })
+    .catch((e) => console.log("Error getting Microsoft description", e));
+}
+
+// #### Tooltips
+//copy text
+function copyText() {
+  var copyText = $("#altBuilding");
+  copyText.select();
+  document.execCommand("copy");
+  var tooltip = $("#myTooltip");
+  tooltip.html("Copied: " + copyText.val());
+}
+
+//Display tooltip changes
+function outFunc() {
+  var tooltip = $("#myTooltip");
+  tooltip.html("Copy to clipboard");
 }
